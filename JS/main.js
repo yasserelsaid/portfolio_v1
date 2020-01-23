e822c27aa6af72cd1c79cf13595948cf28725c60
Yasser <you@example.com>
2020-01-22 20:31:17 -0500

    smooth scrolling without jquery

--- JS/main.js
+++ JS/main.js
@@ -8,25 +8,25 @@ window.addEventListener('scroll', function(){
 
 //smooth scrolling
 
-$('#navbar a, .btn').on('click', function(event){
-  if(this.hash!==''){
-    event.preventDefault();
+// $('#navbar a, .btn').on('click', function(event){
+//   if(this.hash!==''){
+//     event.preventDefault();
 
-    const hash = this.hash;
+//     const hash = this.hash;
 
-    $('html, body').animate(
-      {
-        scrollTop: $(hash).offset().top -100
-      },
-      800
-    );
-  }
+//     $('html, body').animate(
+//       {
+//         scrollTop: $(hash).offset().top -100
+//       },
+//       800
+//     );
+//   }
 
 
-});
+// });
 
-const elsaid = document.querySelectorAll('#elsaid path');
+// const elsaid = document.querySelectorAll('#elsaid path');
 
-for(let i = 0; i<elsaid.length; i++){
-  console.log(`letter ${i} is ${elsaid[i].getTotalLength()}`);
-}
+// for(let i = 0; i<elsaid.length; i++){
+//   console.log(`letter ${i} is ${elsaid[i].getTotalLength()}`);
+// }
