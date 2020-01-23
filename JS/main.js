86b68568a0291a59b0234b4e9d5707086308b4f6
Yasser <you@example.com>
2020-01-22 19:51:40 -0500

    initial commit

---
+++ JS/main.js
@@ -0,0 +1,32 @@
+window.addEventListener('scroll', function(){
+  if(window.scrollY > 150){
+    document.querySelector('#navbar').style.opacity=0.9;
+  }else{
+    document.querySelector('#navbar').style.opacity=1;
+  }
+});
+
+//smooth scrolling
+
+$('#navbar a, .btn').on('click', function(event){
+  if(this.hash!==''){
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top -100
+      },
+      800
+    );
+  }
+
+
+});
+
+const elsaid = document.querySelectorAll('#elsaid path');
+
+for(let i = 0; i<elsaid.length; i++){
+  console.log(`letter ${i} is ${elsaid[i].getTotalLength()}`);
+}
